d9fbd3bef7919a3d2e675ce7463c2ad75bc0d937
Mitikx <T.SAN-JUAN@lprs.fr>
2024-09-30 15:57:37 +0200

    Fix : Route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\ForumController;
-=======
 use App\Http\Controllers\AccueilController;
->>>>>>> f209aff26d9b4fd828e5cb27e5f004bfeb09f4d9
 use App\Http\Controllers\HopitalController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActiviteController;
@@ -12,8 +9,6 @@ use App\Http\Controllers\EtablissementController;
 use App\Http\Controllers\EntrepriseController;
 use App\Http\Controllers\EvenementController;
 use App\Http\Controllers\OffreController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\JobOfferController;
 use App\Http\Controllers\PostController;
 
 // Route vers les offres d'emploi
@@ -30,8 +25,8 @@ Route::resource('evenement', EvenementController::class);
 Route::resource('entreprise', EntrepriseController::class);
 Route::resource('etablissement', EtablissementController::class);
 Route::resource('activite', ActiviteController::class);
-Route::get('joboffers', [JobOfferController::class, 'index'])->name('joboffers.index');
-Route::get('activity', [NewsController::class, 'index'])->name('activity.index');
+Route::get('joboffers', [OffreController::class, 'index'])->name('joboffers.index');
+Route::get('activity', [ActiviteController::class, 'index'])->name('activity.index');
 Route::get('chat', [ForumController::class, 'index'])->name('chat.index');
 
 Route::middleware([
