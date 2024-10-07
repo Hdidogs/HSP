61a01db54f6f3bbb417ba90437b72905e9cc2f58
saam <nospher4z@gmail.com>
2024-10-07 14:34:19 +0200

    Interace create & css fait

--- resources/views/activite/create.blade.php
+++ resources/views/activite/create.blade.php
@@ -1,22 +1,56 @@
 <x-app-layout>
-        <h1>Créer une activité</h1>
-        @if ($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-       @endif
-        <form action="{{ route('activite.store') }}" method="POST">
-            @csrf
-            <label for="titre">Titre :</label>
-            <input type="text" name="titre" id="titre" value="{{ old('titre') }}" required>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Créer une nouvelle activité') }}
+        </h2>
+    </x-slot>
 
-            <label for="desc">Description :</label>
-            <textarea name="desc" id="desc">{{ old('desc') }}</textarea>
-
-            <label for="nb_place">Nombre de places :</label>
-            <input type="number" name="nb_place" id="nb_place" value="{{ old('nb_place') }}" required>
-            <button type="submit">Créer</button>
-        </form>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <form action="{{ route('activite.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="titre">
+                            Titre
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="titre" type="text" name="titre" required>
+                    </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="desc">
+                            Description
+                        </label>
+                        <textarea
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="desc" name="desc" rows="4" required></textarea>
+                    </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="nb_place">
+                            Nombre de places
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="nb_place" type="number" name="nb_place" min="1" required>
+                    </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="date">
+                            Date et Heure
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="date" type="datetime-local" name="date" required>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <button
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit">
+                            Créer l'activité
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
