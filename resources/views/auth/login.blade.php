4cabf01eea1799d0fc8f9618921fb202f6435a64
saam <nospher4z@gmail.com>
2024-11-18 16:27:50 +0100

    space

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,16 +41,17 @@
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-                @if (Route::has('register'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        href="{{ route('register') }}">
-                        {{ __('Register') }}
-                    </a>
-                @endif
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('register'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </a>
+                    @endif
+                    <x-button class="ms-4">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>
